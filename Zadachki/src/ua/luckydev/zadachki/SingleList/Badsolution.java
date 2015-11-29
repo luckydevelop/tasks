5b921943cc1eaeb2cba6b3e83d6188c327375843
Oleg Goncharenko <luckydev3000@gmail.com>
2015-11-29 14:53:57 +0200

    4

---
+++ Zadachki/src/ua/luckydev/zadachki/SingleList/Badsolution.java
@@ -0,0 +1,54 @@
+package ua.luckydev.zadachki.SingleList;
+
+/**
+ * Created by Lucky on 29.11.2015.
+ */
+/*
+public class Badsolution
+{
+    public class InvertList() // () - ??????
+    {
+        public static void Main (String args[]) // - (String[] args)
+
+        {
+            SingleList sl = new SingleList(); //???? ??????????? ????????????? ??????
+
+            sl.add(1); //??????? ????????
+            sl.add(2);
+            sl.add(3);
+            sl.add(4);
+
+            sl = invert(sl);
+        }
+
+    public static SinglList invert(SingleList sl) // SinglEList
+    {
+        ListElement element = sl.head;
+
+        while(element!=sl.tail)
+        {
+            sl.tail.next = sl.head;
+            sl.head = sl.tail;
+            sl.tail = findTail(sl, tail);
+        }
+
+        tail.next = null;
+
+        // ??? return
+    }
+
+    public ListElement findTail(SingleList sl, ListElement tail )
+    {
+        ListElement element = sl.head;
+
+        while(element.next!=tail)
+        {
+            element = element.next;
+        }
+
+        return element;
+
+    }
+}
+}
+*/
