e97b2871006b65b2f75c6f4bc9620487198c0f45
Oleg Goncharenko <luckydev3000@gmail.com>
2015-12-20 09:24:35 +0200

    not bad

---
+++ PrimeFaces/temp/src/main/java/ua/luckydev/entity/Worker.java
@@ -0,0 +1,117 @@
+//package ua.luckydev.entity;
+//
+//import javax.persistence.*;
+//import java.util.ArrayList;
+//import java.util.Date;
+//import java.util.List;
+//
+//@Entity
+//@Table(name = "worker")
+//public class Worker  {
+//    @Id
+//    private int id;
+//    private String fio;
+//
+//    @Temporal(TemporalType.DATE)
+//    private Date dateBirth;
+//
+//    @Temporal(TemporalType.DATE)
+//    private Date dateHire;
+//
+//    String position;
+//    String manager;
+//    String info;
+//
+//    @Transient
+//    List<Person> listWorkers = new ArrayList<>();
+//
+//
+//    public String getManager()
+//    {
+//        return manager;
+//    }
+//
+//    public void setManager(String manager)
+//    {
+//        this.manager = manager;
+//    }
+//
+//    public List<Person> getListWorkers()
+//    {
+//        return listWorkers;
+//    }
+//
+//    public void setListWorkers(List<Person> listWorkers)
+//    {
+//        this.listWorkers = listWorkers;
+//    }
+//
+//    public String getInfo()
+//    {
+//        return info;
+//    }
+//
+//    public void setInfo(String info)
+//    {
+//        this.info = info;
+//    }
+//
+//    public int getId()
+//    {
+//        return id;
+//    }
+//
+//    public Date getDateHire()
+//    {
+//        return dateHire;
+//    }
+//
+//    public void setDateHire(Date dateHire)
+//    {
+//        this.dateHire = dateHire;
+//    }
+//
+//    public String getPosition()
+//    {
+//        return position;
+//    }
+//
+//    public void setPosition(String position)
+//    {
+//        this.position = position;
+//    }
+//
+//    public Date getDateBirth()
+//    {
+//        return dateBirth;
+//    }
+//
+//    public void setDateBirth(Date dateBirth)
+//    {
+//        this.dateBirth = dateBirth;
+//    }
+//
+//    public String getFio()
+//    {
+//        return fio;
+//    }
+//
+//    public void setFio(String fio)
+//    {
+//        this.fio = fio;
+//    }
+//
+//    @Override
+//    public String toString()
+//    {
+//        return "Person{" +
+//                "id=" + id +
+//                ", fio='" + fio + '\'' +
+//                ", position='" + position + '\'' +
+//                '}';
+//    }
+//
+//
+//
+//
+//}
